3f369480341427ab6940ba3055569fec10d8f05b
WijeAkshi <53735848+WijeAkshi@users.noreply.github.com>
2020-05-05 08:28:40 +0530

    Model class edited and JavaScript edited

--- Payment_MS/WebContent/Components/Payments.js
+++ Payment_MS/WebContent/Components/Payments.js
@@ -1,3 +1,149 @@
-/**
- * 
- */
+$(document).ready(function() {
+	if ($("#alertSuccess").text().trim() == "") {
+		$("#alertSuccess").hide();
+	}
+	$("#alertError").hide();
+});
+// SAVE ============================================
+$(document).on("click", "#btnSave", function(event) {
+
+	// Clear alerts---------------------
+	$("#alertSuccess").text("");
+	$("#alertSuccess").hide();
+	$("#alertError").text("");
+	$("#alertError").hide();
+
+	// Form validation-------------------
+	var status = validateItemForm();
+	if (status != true) {
+		$("#alertError").text(status);
+		$("#alertError").show();
+		return;
+	}
+	// If valid------------------------
+	var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT";
+
+	$.ajax({
+		url : "PaymentDetailsAPI",
+		type : type,
+		data : $("#formItem").serialize(),
+		dataType : "text",
+		complete : function(response, status) {
+			onItemSaveComplete(response.responseText, status);
+		}
+	});
+});
+
+function onItemSaveComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success") {
+			$("#alertSuccess").text("Successfully saved.");
+			$("#alertSuccess").show();
+			$("#divItemsGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error") {
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error") {
+		$("#alertError").text("Error while saving.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while saving..");
+		$("#alertError").show();
+	}
+	$("#hidItemIDSave").val("");
+	$("#formItem")[0].reset();
+}
+// UPDATE==========================================
+$(document).on(
+		"click",
+		".btnUpdate",
+		function(event) {
+			$("#hidIDSave").val(
+					$(this).closest("tr").find('#hidIDUpdate').val());
+			$("#app_Id").val($(this).closest("tr").find('td:eq(0)').text());
+			$("#ctype").val($(this).closest("tr").find('td:eq(1)').text());
+			$("#name").val($(this).closest("tr").find('td:eq(2)').text());
+			$("#cardno").val($(this).closest("tr").find('td:eq(3)').text());
+			$("#pho").val($(this).closest("tr").find('td:eq(4)').text());
+			$("#expdate").val($(this).closest("tr").find('td:eq(5)').text());
+			$("#amount").val($(this).closest("tr").find('td:eq(6)').text());
+			$("#status").val($(this).closest("tr").find('td:eq(7)').text());
+		
+		});
+
+// REMOVE ====================================================
+
+$(document).on("click", ".btnRemove", function(event) {
+	$.ajax({
+		url : "PaymentDetailsAPI",
+		type : "DELETE",
+		data : "id=" + $(this).data("id"),
+		dataType : "text",
+		complete : function(response, status) {
+			onItemDeleteComplete(response.responseText, status);
+		}
+	});
+});
+function onItemDeleteComplete(response, status) {
+	if (status == "success") {
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success") {
+			$("#alertSuccess").text("Successfully deleted.");
+			$("#alertSuccess").show();
+			$("#divItemsGrid").html(resultSet.data);
+		} else if (resultSet.status.trim() == "error") {
+			$("#alertError").text(resultSet.data);
+			$("#alertError").show();
+		}
+	} else if (status == "error") {
+		$("#alertError").text("Error while deleting.");
+		$("#alertError").show();
+	} else {
+		$("#alertError").text("Unknown error while deleting..");
+		$("#alertError").show();
+	}
+}
+
+// CLIENTMODEL=========================================================================
+function validateItemForm() {
+	// CODE
+	if ($("#appno").val().trim() == "") {
+		return "Insert Item Code.";
+	}
+	// NAME
+	if ($("#cardtype").val().trim() == "") {
+		return "Insert Item Name.";
+	}
+	// PRICE-------------------------------
+	if ($("#nameOnCard").val().trim() == "") {
+		return "Insert Item Price.";
+	}
+	// is numerical value
+	var tmpPrice = $("#cardno").val().trim();
+	if (!$.isNumeric(tmpPrice)) {
+		return "Insert a numerical value for Item Price.";
+	}
+	// NAME
+	if ($("#phone").val().trim() == "") {
+		return "Insert Phone number.";
+	}
+	// NAME
+	if ($("#expdate").val().trim() == "") {
+		return "Insert Expire date.";
+	}
+	// NAME
+	var tmpPrice = $("#cardno").val().trim();
+	if (!$.isNumeric(tmpPrice)) {
+		return "Insert a numerical value for Item Price.";
+	}
+	// convert to decimal price
+	$("#cardno").val(parseFloat(tmpPrice).toFixed(2));
+	// DESCRIPTION------------------------
+	//if ($("#itemDesc").val().trim() == "") {
+	//	return "Insert Item Description.";
+	}
+	return true;
+
+
--- Payment_MS/src/model/PaymentDetails.java
+++ Payment_MS/src/model/PaymentDetails.java
@@ -9,8 +9,8 @@ import Util.DB_Connection;
 public class PaymentDetails {
 
 	// insert new payment details
-	public String insertPaymentDetails(String appno, String ctype, String name, String cardno, String pho, String expdate,
-			String amount, String status) {
+	public String insertPaymentDetails(String app_Id, String ctype, String name, String cardno, String pho,
+			String expdate, String amount, String status) {
 		String output = "";
 		try {
 			DB_Connection con = new DB_Connection();
@@ -20,26 +20,26 @@ public class PaymentDetails {
 				return "Error while connecting to the database for inserting.";
 			}
 			// create a prepared statement
-			String query = " insert into paymentdetails (`id`,`appno`,`cardType`,`nameOnCard`,`cardno`,`phone`,`expdate`,`amount`)"
+			String query = " insert into paymentdetails (`id`,`app_Id`,`cardType`,`nameOnCard`,`cardno`,`phone`,`expdate`,`amount`)"
 					+ " values (?,?, ?, ?, ?, ?, ?, ?)";
 			PreparedStatement preparedStmt = conn.prepareStatement(query);
 			// binding values
 			preparedStmt.setInt(1, 0);
-			preparedStmt.setString(2, appno);
+			preparedStmt.setString(2, app_Id);
 			preparedStmt.setString(3, ctype);
 			preparedStmt.setString(4, name);
 			preparedStmt.setString(5, cardno);
 			preparedStmt.setString(6, pho);
 			preparedStmt.setDate(7, java.sql.Date.valueOf(expdate));
 			preparedStmt.setDouble(8, Double.parseDouble(amount));
-			
 
 			// execute the statement
 			preparedStmt.execute();
 			conn.close();
-			output = "Inserted successfully";
+			String newPaymentDetails  = readPaymentDetails();
+			output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
 		} catch (Exception e) {
-			output = "Error while inserting the card details.";
+			output = "{\"status\":\"error\", \"data\": \"Error while inserting payment details.\"}";
 			System.err.println(e.getMessage());
 		}
 		return output;
@@ -55,14 +55,14 @@ public class PaymentDetails {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
-			output = "<table border=\"1\"><tr> <th>AppoID</th> <th>CarsType</th> <th>Name</th> <th>cardno</th> <th>Phone</th ><th>Exp_date</th> <th>Amount</th> <th>Status</th> </tr>";
+			output = "<table border=\"1\"><tr> <th>AppoID</th> <th>CarsType</th> <th>Name</th> <th>cardno</th> <th>Phone</th ><th>Exp_date</th> <th>Amount</th> <th>Status</th><th>Update</th><th>Remove</th> </tr>";
 			String query = "select * from paymentdetails";
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			// iterate through the rows in the result set
 			while (rs.next()) {
 				String id = Integer.toString(rs.getInt("id"));
-				String appno = rs.getString("appno");
+				String app_Id = rs.getString("app_Id");
 				String cardType = rs.getString("cardType");
 				String nameOnCard = rs.getString("nameOnCard");
 				String cardno = rs.getString("cardno");
@@ -71,7 +71,8 @@ public class PaymentDetails {
 				String amount = Double.toString(rs.getDouble("amount"));
 				String status = rs.getString("status");
 				// Add into the html table
-				output += "<tr><td>" + appno + "</td>";
+				output += "<tr><td><input id='hidIDUpdate' name='hidIDUpdate' type='hidden' value='" + id + "'>"
+				 + app_Id + "</td>";
 				output += "<td>" + cardType + "</td>";
 				output += "<td>" + nameOnCard + "</td>";
 				output += "<td>" + cardno + "</td>";
@@ -79,6 +80,10 @@ public class PaymentDetails {
 				output += "<td>" + expdate + "</td>";
 				output += "<td>" + amount + "</td>";
 				output += "<td>" + status + "</td>";
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-itemid='"
+						+ id + "'>" + "</td></tr>";
 
 			}
 			conn.close();
@@ -92,8 +97,7 @@ public class PaymentDetails {
 	}
 
 	// update the status of payments
-	public String updatePaymentDetails(String id, String appno, String ctype, String name, String cardno, String pho,
-			String expdate, String amount, String status) {
+	public String updatePaymentDetails(String id, String app_Id, String ctype, String name, String cardno, String pho, String expdate, String amount, String status) {
 		String output = "";
 		try {
 			DB_Connection con = new DB_Connection();
@@ -102,10 +106,10 @@ public class PaymentDetails {
 				return "Error while connecting to the database for updating.";
 			}
 			// create a prepared statement
-			String query = "UPDATE paymentdetails SET appno=?,cardType=?,nameOnCard=?,cardno=?,phone=?,expdate=?,amount=?,status=? WHERE id=?";
+			String query = "UPDATE paymentdetails SET app_Id=?,cardType=?,nameOnCard=?,cardno=?,phone=?,expdate=?,amount=?,status=? WHERE id=?";
 			PreparedStatement preparedStmt = conn.prepareStatement(query);
 			// binding values
-			preparedStmt.setString(1, appno);
+			preparedStmt.setString(1, app_Id);
 			preparedStmt.setString(2, ctype);
 			preparedStmt.setString(3, name);
 			preparedStmt.setString(4, cardno);
@@ -118,9 +122,10 @@ public class PaymentDetails {
 			// execute the statement
 			preparedStmt.execute();
 			conn.close();
-			output = "Updated successfully";
+			String newPaymentDetails = readPaymentDetails();
+			output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
 		} catch (Exception e) {
-			output = "Error while updating the card details.";
+			output = "{\"status\":\"error\", \"data\": \"Error while updating the payment details.\"}";
 			System.err.println(e.getMessage());
 		}
 		return output;
@@ -143,41 +148,41 @@ public class PaymentDetails {
 			// execute the statement
 			preparedStmt.execute();
 			conn.close();
-			output = "Deleted successfully";
+			String newPaymentDetails = readPaymentDetails();
+			output = "{\"status\":\"success\", \"data\": \"" + newPaymentDetails + "\"}";
 		} catch (Exception e) {
 			output = "Error while deleting the card details.";
 			System.err.println(e.getMessage());
 		}
 		return output;
 	}
-
+/*
 	// retrieve the appointment details with the payment amount
 	public String readAppointmentDetails() {
-	
-		
+
 		String output = "";
 		try {
 			DB_Connection con = new DB_Connection();
 			Connection conn = con.connect();
-			
+
 			if (con == null) {
 				return "Error while connecting to the database for reading.";
 			}
 			// Prepare the html table to be displayed
 			output = "<table border=\"1\"><tr> <th>AppoID</th> <th>date</th> <th>Name</th> <th>doctor id</th> <th>doc fee</th ><th>hospital fee</th>  <th>total</th></tr>";
-			String query = "select a.apno,a.date,a.patient,a.doctor,d.fee  from doctor d,appointment a where a.doctor= d.did ";
+			String query = "select a.app_Id,a.date,a.patient,a.doctor,d.fee  from doctor d,appointment a where a.doctor= d.did ";
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			// iterate through the rows in the result set
 			while (rs.next()) {
-				String apno = rs.getString("apno");
+				String app_Id = rs.getString("app_Id");
 				String date = rs.getString("date");
 				String patient = rs.getString("patient");
 				String doctor = rs.getString("doctor");
 				String fee = Double.toString(rs.getDouble("fee"));
 				String amount = fee + 2000;
 				// Add into the html table
-				output += "<tr><td>" + apno + "</td>";
+				output += "<tr><td>" + app_Id + "</td>";
 				output += "<td>" + date + "</td>";
 				output += "<td>" + patient + "</td>";
 				output += "<td>" + doctor + "</td>";
@@ -195,6 +200,5 @@ public class PaymentDetails {
 		}
 		return output;
 	}
-
-
+*/
 }
